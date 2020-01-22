25a8e122e5f6c95663ad0e4c0f1e69226c0ce9b6
rohit1818 <srohit@cse.iitb.ac.in>
2020-01-22 20:40:01 +0530

    Changed one line SEIL_Energy.py

--- SEIL_Energy.py
+++ SEIL_Energy.py
@@ -5,7 +5,7 @@ model=load_model("LSTM30.h5")
 
 def energy_pred_LSTM(rowtestx):
     rowtestx=np.array([rowtestx])
-    rowtestx=np.reshape(rowtestx,(rowtestx.shape[0],1,rowtestx.shape[1]))
+    rowtestx=np.reshape(rowtestx,(rowtestx.shape[0],rowtestx.shape[1],1))
     pred=model.predict(rowtestx)
     return pred[0][0]
     
